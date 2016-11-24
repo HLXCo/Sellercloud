aeab31b9caa614fc38fbf48a8306711af0e7fe7d
HLXCo <sergio@hlx.co>
2016-11-24 14:01:23 -0500

    Modified Sellercloud

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Sergio Molanes',
   author_email = 'sergio@hlx.co',
   url = 'https://github.com/HLXCo/Sellercloud', # use the URL to the github repo
-  download_url = 'https://github.com/HLXCo/Sellercloud/tarball/0.1.1', # I'll explain this in a second
-  keywords = ['selling', 'sellercloud', 'soap'], # arbitrary keywords
-  classifiers = [],
+  download_url = 'https://github.com/HLXCo/Sellercloud/tarball/0.1.1', 
+  keywords = ['selling', 'sellercloud', 'wsdl','api','web services','oms','erp','multi-channel fulfillement','repricing','inventory management'],
+  classifiers = []
 )
